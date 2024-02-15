6538790450750c6061efaa6a51a43d50f3420b29
davideaded <david.castrodc033@gmail.com>
2024-02-15 11:11:20 -0300

    Identify if square is clicked again. Improvements to the square state

--- src/components/MainImage.jsx
+++ src/components/MainImage.jsx
@@ -3,23 +3,49 @@ import '../styles/mainimage.css';
 import { useState } from 'react';
 
 export default function MainImage() {
-	const [square, setSquare] = useState(null);
+	const [square, setSquare] = useState({});
+	const squareSize = 56;
 	const moonCoordinates = { x: 284, y: 170 };
 
 	const handleClick = (e) => {
 		const {pageX, pageY} = e;
-		const top = pageY - 50/2;
-		const left = pageX - 50/2;
+		const top = pageY - squareSize/2;
+		const left = pageX - squareSize/2;
+
+		if (square.coordinates && isCoordinateInsideSquare(left, top, square)) {
+			console.log(true);
+			return true;
+		}
 
 		console.log(pageX, pageY);
-		setSquare(<div className="targetArea" style={{ left: left, top: top }}>{pageX}, {pageY}</div>);
+		setSquare((p) => ({
+					...p,
+					coordinates: { left, top },
+					element: (
+						<div className="targetArea" style={{ left: left, top: top }}>
+							{pageX}, {pageY}
+						</div>
+					),
+		}));
 	}
+
+	const isCoordinateInsideSquare = (x, y, square) => {
+		const squareLeft = square.coordinates.left;
+		const squareTop = square.coordinates.top;
+
+		return (
+			x >= squareLeft &&
+			x <= squareLeft + squareSize &&
+			y >= squareTop &&
+			y <= squareTop + squareSize
+		);
+	};
 
 	return (
 		<>
-			<div className="container">
-				{square && square}
-				<img onClick={(e) => handleClick(e)} src={waldo} />
+			<div className="container"  >
+				{square && square.element}
+				<img src={waldo} onClick={(e) => handleClick(e)}/>
 			</div>
 		</>
 	)
