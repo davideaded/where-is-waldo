b3f0cf758b9bdc71935e1b38429d56d0444a3d0b
davideaded <david.castrodc033@gmail.com>
2024-02-17 11:30:08 -0300

    Added routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import MainImage from './components/MainImage.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "game",
+    element: <MainImage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+		<RouterProvider router={router} />
   </React.StrictMode>,
 )
